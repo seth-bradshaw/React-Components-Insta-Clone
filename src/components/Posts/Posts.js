a6afd53ce2b0530de97e3c0029d6601656d50c26
seth-bradshaw <seth.bradshaw27@gmail.com>
2020-10-06 14:58:43 -0600

    Finished assignment

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -9,6 +9,9 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(currentPost => {
+        return <Post likePost={likePost} post={currentPost}/>
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
